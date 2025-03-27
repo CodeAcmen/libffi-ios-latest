583dc21e1c3a9b7869e853c3cff5a5caa57331fa
tangjiapeng <tangjiapeng@itjuzi.com>
2025-03-27 13:07:31 +0800

    Initial Commit

---
+++ libffi-ios-latest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  libffi-ios-latest
+//
+//  Created by 唐佳鹏 on 2025/3/27.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
